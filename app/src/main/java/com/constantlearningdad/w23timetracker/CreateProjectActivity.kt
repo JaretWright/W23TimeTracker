27032f6f4ce096c8b8a95d93b716d209152d1cf3
JaretWright <jaret.wright@georgiancollege.ca>
2023-02-07 17:11:59 -0500

    Week 5 - connected up Firestore

--- app/src/main/java/com/constantlearningdad/w23timetracker/CreateProjectActivity.kt
+++ app/src/main/java/com/constantlearningdad/w23timetracker/CreateProjectActivity.kt
@@ -2,7 +2,10 @@ package com.constantlearningdad.w23timetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.constantlearningdad.w23timetracker.databinding.ActivityCreateProjectBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CreateProjectActivity : AppCompatActivity() {
     private lateinit var binding : ActivityCreateProjectBinding
@@ -11,5 +14,48 @@ class CreateProjectActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateProjectBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.createProjectButton.setOnClickListener {
+            //if the fields are populated, create a Project object
+            //and store it in firestore
+            var projectName = binding.projectNameEditText.text.toString()
+            var description = binding.descriptionEditText.text.toString()
+
+            if (projectName.isNotEmpty() && description.isNotEmpty())
+            {
+                //create a project and send to firestore
+                var project = Project(projectName, description)
+
+                //connect to Firebase-Firestore
+                //if the collection doesn't exist, it will add it
+                //if the collection exists, it will access it
+                val db = FirebaseFirestore.getInstance().collection("projects")
+
+                //save the project as a document
+                val id = db.document().getId()
+                project.id = id
+
+                db.document().set(project)
+                    .addOnSuccessListener {
+                        Toast.makeText(this,"DB Updated",Toast.LENGTH_LONG).show()
+                        binding.projectNameEditText.text.clear()
+                        binding.descriptionEditText.text.clear()
+
+                        //read from the DB and log the projects
+                        db.get().addOnSuccessListener { collection ->
+                            for (document in collection)
+                            {
+                                Log.i("Firestore","${document.id}=>${document.data}")
+                            }
+                        }
+
+                    }
+                    .addOnFailureListener { exception -> Log.w("DB_Issue",exception.localizedMessage) }
+            }
+            else
+            {
+                Toast.makeText(this, "name and description are both required",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
