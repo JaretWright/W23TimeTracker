9492f9859a21f91126d83ed5f26dc4b801fc0821
JaretWright <jaret.wright@georgiancollege.ca>
2023-03-07 16:26:38 -0500

    Week 8 - Added item_spinner.xml

--- app/src/main/java/com/constantlearningdad/w23timetracker/LogTimeActiviy.kt
+++ app/src/main/java/com/constantlearningdad/w23timetracker/LogTimeActiviy.kt
@@ -3,6 +3,7 @@ package com.constantlearningdad.w23timetracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import com.constantlearningdad.w23timetracker.databinding.ActivityLogTimeActiviyBinding
 
 class LogTimeActiviy : AppCompatActivity() {
@@ -15,5 +16,8 @@ class LogTimeActiviy : AppCompatActivity() {
 
         var documentID = intent.getStringExtra("documentID")
         Log.i("documentID",documentID.toString())
+
+        val viewModel : ProjectViewModel by viewModels()
+        viewModel.getProjects()
     }
 }
